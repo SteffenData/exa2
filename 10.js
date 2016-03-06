2626b7202598ae96e5f95d9c521f0b79759507e5
juhlmadsen1982@gmail.com <juhlmadsen1982@gmail.com>
2016-03-06 16:35:11 +0100

    answar 10 and more

--- 10.js
+++ 10.js
@@ -2,4 +2,25 @@
  * Created by steffen on 06-03-2016.
  */
 //Explain, using relevant examples, different ways to mock out databases, HTTP-request etc.
+// mock our database
 
+// we need to use the module nock
+// var nock = require('nock') that does it able to test for urls without addiing anything to the database
+//when we use the nock like below
+
+var nock = require('nock');
+var thissitetointercept = ('http://myproject.com')
+    .post('/user',{
+        username:'flamingo',
+        email: 'something.com'
+    })
+    .reply(201,{
+    ok: true,
+    id:'123',
+    rev:'4685'
+})
+
+// we say that we will intercept any urls to http://myproject.com'
+// with the postrequest user: email:
+// after that line we can begin the test of our api without adding the user to our database
+//
--- 8.js
+++ 8.js
@@ -3,3 +3,4 @@
  */
 // Explain, using a relevant examples, your strategy for implementing a REST-API with Node/Express and show
 // how you can "test" all the four CRUD operations programmatically using for example the Request package.
+// need more to know more to answer to this:
